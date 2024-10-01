b08e75b52ae35949a9b2a4a3405ec69346ffd9e6
LarissaFurtadoOnido <olarissafurtado@gmail.com>
2024-10-01 11:21:11 -0300

    up

--- manipulacao_objeto/script.js
+++ manipulacao_objeto/script.js
@@ -4,14 +4,37 @@ dados em uma tabela e, ao clicar em um botão, permita a atualização das infor
 através de um formulário.
 */
 
+const Pessoa = []
+
 const btn = document.querySelector("#modificar")
 const atualizar = document.querySelector("#atualizar")
+const popup = document.querySelector("dialog")
+const tabela = document.querySelector("#novo")
 
 btn.addEventListener("click", function(evento){
     evento.preventDefault()
-
-    const popup = document.querySelector("dialog")
     popup.showModal()
+})
+
+atualizar.addEventListener("click", function(evento){
+    evento.preventDefault()
+
+    const nome = document.querySelector("#nome").value
+    const idade = document.querySelector("#idade").value
+    const cidade = document.querySelector("#cidade").value
+
+    let novaLinha = tabela.insertRow(-1)
+    Pessoa.push({nome, idade, cidade})
+
+    let cellNome = novaLinha.insertCell(0);
+    let cellIdade = novaLinha.insertCell(1);
+    let cellCidade = novaLinha.insertCell(2);
+
+    cellNome.textContent = nome;
+    cellIdade.textContent = idade;
+    cellCidade.textContent = cidade;
+
+    popup.close()
 })
 
 
