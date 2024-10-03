593ff8bc32277fdcaecdc8937070c4777638d839
LarissaFurtadoOnido <olarissafurtado@gmail.com>
2024-10-03 00:52:22 -0300

    tentei

--- manipulacao_objeto/script.js
+++ manipulacao_objeto/script.js
@@ -6,14 +6,15 @@ através de um formulário.
 
 const Pessoa = []
 
-const btn = document.querySelector("#modificar")
+const modificar = document.querySelector("#modificar")
 const atualizar = document.querySelector("#atualizar")
-const popup = document.querySelector("dialog")
+const popup = document.querySelector("#popup")
 const tabela = document.querySelector("#novo")
 
-btn.addEventListener("click", function(evento){
+modificar.addEventListener("click", function(evento){
     evento.preventDefault()
     popup.showModal()
+
 })
 
 atualizar.addEventListener("click", function(evento){
@@ -23,16 +24,7 @@ atualizar.addEventListener("click", function(evento){
     const idade = document.querySelector("#idade").value
     const cidade = document.querySelector("#cidade").value
 
-    let novaLinha = tabela.insertRow(-1)
-    Pessoa.push({nome, idade, cidade})
-
-    let cellNome = novaLinha.insertCell(0);
-    let cellIdade = novaLinha.insertCell(1);
-    let cellCidade = novaLinha.insertCell(2);
-
-    cellNome.textContent = nome;
-    cellIdade.textContent = idade;
-    cellCidade.textContent = cidade;
+    
 
     popup.close()
 })
